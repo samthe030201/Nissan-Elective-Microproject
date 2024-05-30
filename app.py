b6318c9cfc536562601d6d429598fd4bca6c68da
samthe030201 <sampthenankal@gmail.com>
2024-05-31 01:00:42 +0530

    commit 8

--- app.py
+++ app.py
@@ -1,25 +1,58 @@
 from flask import Flask, request, jsonify
-import joblib
+from flask_cors import CORS
+import pickle
 import numpy as np
 
+
 app = Flask(__name__)
+CORS(app)
 
-model = joblib.load('xg_model.sav')
+model = pickle.load(open('xg_model.sav', 'rb'))
 
-# Define a function to map the prediction to popularity range
-def map_prediction_to_popularity(prediction):
-    return f'Popularity of Artist and Song is between {prediction*20}-{(prediction+1)*20}'
+'''print(model.get_booster().feature_names)
+example_features = [3, 1, 200000, 0.5, 0.8, 0.7, 0.0, 0.2, -5, 0.3, 120, 0.6, 1, 1, 4]
+features_array = np.array([example_features], dtype=np.float32)
+
+# Print the shape of the features array
+print("Features array shape:", features_array.shape)
+
+print(features_array)
+
+# Make prediction
+try:
+    prediction = model.predict(features_array)
+    print("Prediction:", prediction)
+except ValueError as e:
+    print("ValueError:", e)'''
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    input_features = np.array([[data['popularity'], data['markets'], data['duration_ms'], data['acousticness'],
-                                data['danceability'], data['energy'], data['instrumentalness'], data['liveness'],
-                                data['loudness'], data['speechiness'], data['tempo'], data['valence'],
-                                data['musicalkey'], data['musicalmode'], data['time_signature']]])
-    prediction = model.predict(input_features)[0]
-    popularity_range = map_prediction_to_popularity(prediction)
-    return jsonify({'prediction': popularity_range})
+    data = request.json
+    
+    features = [int(data['markets']), int(data['duration_ms']), float(data['acousticness']), 
+                float(data['danceability']), float(data['energy']), float(data['instrumentalness']), float(data['liveness']), 
+                float(data['loudness']), float(data['speechiness']), float(data['tempo']),float(data['valence']), 
+                int(data['musicalkey']), int(data['musicalmode']), int(data['time_signature']),int(data['count'])]
+    
+    #print(features)
+    
+    features_array = np.array([features], dtype=np.float32)
+
+    #print(features_array)
+   
+    prediction = model.predict(features_array)
+
+    
+    mapping = {
+        0: 'Popularity of Artist and Song is between 0-20',
+        1: 'Popularity of Artist and Song is between 21-40',
+        2: 'Popularity of Artist and Song is between 41-60',
+        3: 'Popularity of Artist and Song is between 61-80',
+        4: 'Popularity of Artist and Song is between 81-100'
+    }
+    result = mapping[prediction[0]]
+    #result = 1
+    return jsonify({'prediction': result})
 
 if __name__ == '__main__':
     app.run(debug=True)
